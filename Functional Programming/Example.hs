9fc1528eacbdb6658c17f28cccfec4eb75aea2ba
nwoik <nwokoyed@gmail.com>
2022-09-23 09:37:16 +0100

    added

---
+++ Functional Programming/Example.hs
@@ -0,0 +1,11 @@
+module Example where 
+
+data ABC = A Int [Int] 
+fancy :: Num int => [int]
+fancy = [1,2,3]
+
+friendly :: [Char]
+friendly = "Hello"
+
+add_dot :: String -> String
+add_dot string = '.' : string
