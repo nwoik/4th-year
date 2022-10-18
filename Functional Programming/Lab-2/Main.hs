ea8721e883e0e9904212ffd637d1d5e0eb3c9d5c
nwoik <nwokoyed@gmail.com>
2022-10-18 18:55:42 +0100

    dong

--- Functional Programming/Lab-2/Main.hs
+++ Functional Programming/Lab-2/Main.hs
@@ -41,7 +41,10 @@ The tail of the list is a recursive call,
 with the tails of both is and ts.
 
 Else if the fst t isn't equal to i,
-then selections is called with (i:is) and the tail of ts.
+then selections is called with (t:ts) 
+and the tail of is to move the tuples along.
+Else then selections is called with (i:is) 
+and the tail of ts to move the indices along.
 -}
 
 selections :: [Int] -> [(Int, Int)] -> [Int]
@@ -50,6 +53,7 @@ selections [] (t:ts) = []
 selections (i:is) [] = []
 selections (i:is) (t:ts) = 
     if fst t == i then (snd t : selections is ts)
+    else if fst t < i then selections is (t:ts)
     else selections (i:is) ts
 
 
@@ -74,16 +78,22 @@ concatenations [as] = as
 concatenations ((b:bs):as) = [b ++ l | b <- (b:bs), 
         l <- concatenations [a | a <- as]]
 
-
 {- Task 4
 
 -}
 
 k_ary_patterns :: Int -> [(Int, [[Int]])]
-k_ary_patterns 0 = [];
-k_ary_patterns n = ((n,bs): k_ary_patterns (n-1))
-    where bs = [[n ^ n]]
+k_ary_patterns n = 
+k_ary_patterns n = [(a, ls) | a <- take (n) [i | i <- [1..]]]
+    where ls = take (n-1) [[j] | j <- [0..]]
 
+
+
+listOfNLists :: Int -> [[Int]]
+listOfNLists n = take n [[i] | i <- [0..]]
+
+listOfN :: Int -> [Int]
+listOfN n = take n [i | i <- [1..]]
 
 main = do
     print()
