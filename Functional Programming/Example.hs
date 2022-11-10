a077a23df40afa7ee9d378ba59fd11fcefac663e
nwoik <nwokoyed@gmail.com>
2022-11-10 19:13:12 +0000

    ai

--- Functional Programming/Example.hs
+++ Functional Programming/Example.hs
@@ -1,26 +1,26 @@
 module Example where 
 import Data.Char (ord, chr)
 
-letter_to_int :: Char -> Int 
-letter_to_int c = ord c - ord 'a'
+-- letter_to_int :: Char -> Int 
+-- letter_to_int c = ord c - ord 'a'
 
 
-int_to_letter :: Int -> Char 
-int_to_letter i = chr (i + ord 'a')
+-- int_to_letter :: Int -> Char 
+-- int_to_letter i = chr (i + ord 'a')
 
-shift :: Int -> Char -> Char
-shift i c = chr $ ((ord c - ord 'a') + i) `mod` 26
+-- shift :: Int -> Char -> Char
+-- shift i c = chr $ ((ord c - ord 'a') + i) `mod` 26
 
-encode :: Int -> String -> String
-encode n = map (shift n) 
+-- encode :: Int -> String -> String
+-- encode n = map (shift n) 
 
-percentage :: Int -> Int -> Float
-percentage m n = (fromIntegral m / fromIntegral n) * 100
+-- percentage :: Int -> Int -> Float
+-- percentage m n = (fromIntegral m / fromIntegral n) * 100
 
-count :: Char -> String -> Int
-count c cs = length [c' | c' <- cs, c' == c]
-count c cs = length (filter prd cs)
-    where prd c' = c' == c
+-- count :: Char -> String -> Int
+-- count c cs = length [c' | c' <- cs, c' == c]
+-- count c cs = length (filter prd cs)
+--     where prd c' = c' == c
 
 main = do
     print()
