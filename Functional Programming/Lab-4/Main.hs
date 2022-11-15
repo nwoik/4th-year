3526021b02d42ccf8d7c17520d2d1e71f5356b92
nwoik <nwokoyed@gmail.com>
2022-11-15 17:24:31 +0000

    f

--- Functional Programming/Lab-4/Main.hs
+++ Functional Programming/Lab-4/Main.hs
@@ -17,8 +17,7 @@ Both of the children are BSTs that can be null or it's own sub tree.
 data BST = Null | Node {root::Int,
                         leftchild::BST,
                         rightchild::BST}
-                        deriving (Show)
-
+                    
 
 {- Task 2
 create initializes an empty binary search tree with the root as null.
@@ -48,7 +47,8 @@ make_tree (Node root leftchild rightchild)
 
 -}
 
-
+-- pretty_print' :: BST -> String
+-- pretty_print' bst = show bst
 
 
 {- Task 5
@@ -75,15 +75,23 @@ insert (Node a' left right) a
 
 
 {- Task 7
+from_list takes in a list of ints and returns a BST.
 
+foldl is applied to the insert function (b -> a -> b),
+the BST Null and the list of ints.
+The function insert is applied to the empty BST and every element of as. 
+((Null `insert` a1) `insert` a2) `insert` an
+
+This creates the BST tree.
 -}
 
 
-fromList :: Foldable t => t Int -> BST
-fromList as = foldl insert Null as
+from_list :: [Int] -> BST
+from_list as = foldl insert Null as
 
 main = do
 
     putStrLn ""
+    putStr (pretty_print (from_list [3,5,1,6,4,2,0]))
 
-    putStr (pretty_print (fromList [3,5,1,6,4,2,0]))
+
