519d6c5ccfed83fb9156e287516f0fca0669ad68
nwoik <nwokoyed@gmail.com>
2023-04-10 17:54:02 +0100

    comp

--- Computer Security/login.py
+++ Computer Security/login.py
@@ -1,25 +1,29 @@
-import getpass
-import hashlib
+import matplotlib.pyplot as plot
 
-s_user = "nwoik"
-s_pin  = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
+x = []
+y = []
+bit = 16
 
-def hash_string(message:str):
-    return hashlib.sha256(message.encode("UTF-8")).hexdigest()
+# naming the x axis
+plot.xlabel('Number of Challeges (k)')
+# naming the y axis
+plot.ylabel('Probability (p)')
 
+# giving a title to my graph
+plot.title('Probability of each challenge being Unique')
+prob = 0.0
+def get_prob(N, num_k):
+    pos = num_k
+    act = N - 1.0
+    prob = (num_k - act)/num_k
+    y.append(prob)
 
-def login(user, pin):
-    if user == s_user and s_pin == hash_string(pin):
-        print("user/PIN : " + user + "/" + pin)
-        return True
-    return False
+def get_x_coord(num1, num2):
+    for i in (range(num1, num2 + 1)):
+        x.append(i)
+        get_prob(i, num2)
 
+get_x_coord(1,2**bit)
 
-def main():
-    user = input("user: ")
-    pin = getpass.getpass("PIN :")
-    if login(user, pin):
-        print("access granted")
-
-    else :
-        print("access denied")
+plot.plot(x,y)
+plot.show()
